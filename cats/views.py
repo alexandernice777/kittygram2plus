e774bc81b4c4b08cc29b7d89af250a41b63f8407
Александр Сисецкий <alexandernice777@yandex.ru>
2023-02-28 20:44:31 +0300

    hw update

--- cats/views.py
+++ cats/views.py
@@ -1,16 +1,39 @@
 from rest_framework import viewsets
+from rest_framework import filters
+from rest_framework.throttling import AnonRateThrottle, ScopedRateThrottle
+from django_filters.rest_framework import DjangoFilterBackend
 
 from .models import Achievement, Cat, User
-
+from .permissions import OwnerOrReadOnly, ReadOnly
 from .serializers import AchievementSerializer, CatSerializer, UserSerializer
+from .throttling import WorkingHoursRateThrottle
+from .pagination import CatsPagination
 
 
 class CatViewSet(viewsets.ModelViewSet):
     queryset = Cat.objects.all()
     serializer_class = CatSerializer
+    permission_classes = [OwnerOrReadOnly]
+    throttle_classes = [
+        WorkingHoursRateThrottle, AnonRateThrottle, ScopedRateThrottle
+        ]
+    throttle_scope = 'low_request'
+    pagination_class = None
+    filter_backends = [
+        DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter
+        ]
+    filterset_fields = ['color', 'birth_year']
+    search_fields = ['name']
+    ordering_fields = ['name', 'birth_year']
+    ordering = ['birth_year']
 
     def perform_create(self, serializer):
-        serializer.save(owner=self.request.user) 
+        serializer.save(owner=self.request.user)
+
+    def get_permissions(self):
+        if self.action == 'retrieve':
+            return [ReadOnly]
+        return super().get_permissions()
 
 
 class UserViewSet(viewsets.ReadOnlyModelViewSet):
